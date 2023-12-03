e9d273096e3ceadb61566186ad7a9eb9e1973c45
Abhijit <akm5630@gmail.com>
2023-12-02 21:09:18 -0800

    Switching logic push

--- chess_coach_app.py
+++ chess_coach_app.py
@@ -5,52 +5,49 @@ from export_lichess_games import ChessGameDownloader
 # Initialize session state
 if 'view_results' not in st.session_state:
     st.session_state['view_results'] = False
+if 'new_run' not in st.session_state:
+    st.session_state['new_run'] = False
+if 'existing_run' not in st.session_state:
+    st.session_state['existing_run'] = False
 if 'analysis_folder_path' not in st.session_state:
     st.session_state['analysis_folder_path'] = ""
 
 
-# Main app logic
-def page_switch_logic():
-    print("Switch Logic Active")
-    if st.session_state['view_results']:
-        results_page()
-    else:
-        main_page()
-        
+def existing_run_page():
+    run_id = st.text_input("Run ID:")
+    if st.button("Analyze"):
+        if len(run_id)>0:
+            st.session_state['analysis_folder_path'] = f'games/{run_id}/analysis/'
+            st.session_state['view_results'] = True
+            if st.button("View Results"):
+                pass
+
 # Streamlit UI for the main page
 def main_page():
     st.title("Chess Game Analyzer")
     st.write("Enter your details and date range to analyze chess games.")
 
     username = st.text_input("Username:")
-    api_token = st.text_input("API Token:", type="password")
     start_date = st.date_input("Start Date:")
-    end_date = st.date_input("End Date:")
-    run_id = st.text_input("Run ID:")
 
     if st.button("Analyze"):
         downloader = ChessGameDownloader()
         epoch_time = downloader.date_text_to_epoch(start_date.strftime('%Y-%m-%d'))
-        if len(run_id)>0:
-            status = downloader.run_analysis(run_id=run_id,username=username)
-        else:
-            folder = downloader.fetch_and_save_games(epoch_time)
-            st.success(f"Loaded files successfully to --> {folder}")
+        folder = downloader.fetch_and_save_games(epoch_time)
+        st.success(f"Loaded files successfully to --> {folder}")
 
-            status = downloader.run_analysis()
+        status = downloader.run_analysis()
 
         if status:
             st.success("Analysis completed successfully!")
-            if len(run_id)>0:
-                st.session_state['analysis_folder_path'] = f'games/{run_id}/analysis/'
-            else:
-                st.session_state['analysis_folder_path'] = f'{folder}/analysis/'
+            st.session_state['analysis_folder_path'] = f'{folder}/analysis/'
             st.session_state['view_results'] = True
             if st.button("View Results"):
                 pass
                 
         else:
             st.error("An error occurred during analysis:")
+
 
 
 # Streamlit UI for the results page
@@ -78,8 +75,29 @@ def results_page():
     if st.button("Load Games again!"):
         st.session_state['analysis_folder_path'] = ""
         st.session_state['view_results'] = False
+        st.session_state['existing_run'] = False
+        st.session_state['new_run'] =  False
+        st.experimental_rerun()
+
+# Main app logic
+def page_switch_logic():
+    print("Switch Logic Active")
+    if st.session_state['view_results']:
+        results_page()
+    elif st.session_state['new_run']:
+        main_page()
+    elif st.session_state['existing_run']:
+        print("Here!")
+        existing_run_page()
+    else:
+        options_page()
+
+def options_page():
+    if st.button("New Run"):
+        st.session_state['new_run'] = True 
         page_switch_logic()
-
-
+    elif st.button("Existing Run"):
+        st.session_state['existing_run'] = True
+        page_switch_logic()
 
 page_switch_logic()
